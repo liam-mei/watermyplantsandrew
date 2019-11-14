79d5b78f3ba686b0ba4379a413196a2f2bd48d47
andrewbastian <andrew.bastian@gmail.com>
2019-11-13 19:41:28 -0800

    Redux Frame work in place. Still needs some actions and nothing is tested.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+import Homepage from "./components/Homepage";
+
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+import Dashboard from "./components/Dashboard";
+import PlantCard from "./components/PlantCard";
+import AddPlant from "./components/AddPlant";
+
+const App = () => (
+  <div className="app">
+
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/login" component={Login} />
+        <Route path="/account/register" component={Register} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/plant/add" component={AddPlant} />
+        <PrivateRoute exact path="/plant/:id" component={PlantCard} />
+        <PrivateRoute
+          path="/plant/:id/edit"
+          render={() => <div>Edit Plant</div>}
+        />
+
+      </Switch>
+  </div>
+);
 
 export default App;
---
+++ src/actions/auth.js
@@ -0,0 +1,53 @@
+import { push } from "connected-react-router";
+import API from "../utils/API";
+
+export const LOGIN_REQUEST = "LOGIN_REQUEST";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+
+export const authenticateUser = ({ username, password }) => dispatch => {
+  dispatch({ type: LOGIN_REQUEST });
+
+  API.post("/auth/login", {
+    username,
+    password
+  })
+    .then(response => {
+      localStorage.setItem("token", response.data.token);
+
+      dispatch({ type: LOGIN_SUCCESS });
+      dispatch(push("/dashboard"));
+    })
+    .catch(error =>
+      dispatch({
+        type: LOGIN_FAILURE,
+        errorMessage: error.response.data.message
+      })
+    );
+};
+
+export const REGISTER_REQUEST = "REGISTER_REQUEST";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAILURE = "REGISTER_FAILURE";
+
+export const registerUser = ({ username, password, phone }) => dispatch => {
+  dispatch({ type: REGISTER_REQUEST });
+
+  API.post("/auth/register", {
+    username,
+    password,
+    phone
+  })
+    .then(response => {
+      localStorage.setItem("token", response.data.token);
+
+      dispatch({ type: LOGIN_SUCCESS });
+      dispatch(push("/dashboard"));
+    })
+    .catch(error =>
+      dispatch({
+        type: REGISTER_FAILURE,
+        errorMessage: error.response.data.message
+      })
+    );
+};
