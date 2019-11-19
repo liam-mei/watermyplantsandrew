cbc9938c49963fb181f0a03a7afab155eeed4e8d
jacobcalv <jacobcalvino@outlook.com>
2019-11-18 21:09:23 -0600

    got rid of logout

--- src/components/Logout.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { push } from "connected-react-router";
-export function Logout() {
-    console.log('hi')
-	localStorage.removeItem("token")
-	localStorage.removeItem("userID")
-	return push("/login")
-}
-
