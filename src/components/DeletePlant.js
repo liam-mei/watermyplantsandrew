3e17e530c7f7af16022e737a27684feb1f5404ad
jacobcalv <jacobcalvino@outlook.com>
2019-11-20 20:24:41 -0600

    plant card added

---
+++ src/components/DeletePlant.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+export default function DeletePlant() {
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div>
+      <Button variant="" color="primary" onClick={handleClickOpen}>
+        Delete
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Delete Chinese Evergreen"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you would like to delete this plant? This action cannot be undone. 
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
---
+++ src/components/PlantCard.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import LocalDrinkIcon from '@material-ui/icons/LocalDrink';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import DeletePlant from  "./DeletePlant"
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+});
+export default function PlantCard(props) {
+  const classes = useStyles();
+  return (
+    <div className="items-list-wrapper">
+<Card className={classes.card}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Plant"
+            height="200"
+            image="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1557258847-chinese-evergreen-houseplant-1557258690.jpg?crop=0.883xw:0.887xh;0.0849xw,0.0821xh&resize=480:*"
+            title="Plant"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+            {props.plant.name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+         <AccessTimeIcon color="primary" fontSize="small" /> 1:30pm  <LocalDrinkIcon color="primary" fontSize="small" /> 3oz.
+            </Typography> 
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Edit
+          </Button>
+          <Button size="small" color="primary">
+          <DeletePlant />
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+}
+
