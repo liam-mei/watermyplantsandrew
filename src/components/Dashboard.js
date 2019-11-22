fc293cba2e7f760c498edf17924fa268c3724713
andrewbastian <andrew.bastian@gmail.com>
2019-11-22 15:52:21 -0800

    More Cleaning

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,15 +6,11 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Drawer from "@material-ui/core/Drawer";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import Badge from "@material-ui/core/Badge";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Link from "@material-ui/core/Link";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import NotificationsIcon from "@material-ui/icons/Notifications";
@@ -125,7 +121,7 @@ function Dashboard(props) {
   useEffect(()=>{
     props.getPlants()
     props.getPlantSchedule()
-},[])
+},[props.plants])
 
 console.log(props)
   const classes = useStyles();
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { Link } from "react-router-dom";
